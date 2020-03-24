f2a8d74625d82d43db953032cc516cb2018a911b
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-03-25 10:02:39 +1100

    conf: update config to use my cognito

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
-  aws_project_region: 'eu-central-1',
-  aws_cognito_identity_pool_id: 'eu-central-1:1bc1c606-f159-43e9-b090-117b6983768d',
-  aws_cognito_region: 'eu-central-1',
-  aws_user_pools_id: 'eu-central-1_qynKm3Cn8',
-  aws_user_pools_web_client_id: '68u98au0oq2qotp7neu7haagll',
+  aws_project_region: 'ap-southeast-2',
+  aws_cognito_identity_pool_id: 'ap-southeast-2:e5daeabc-686a-42df-a247-1cfd0b0bc404',
+  aws_cognito_region: 'ap-southeast-2',
+  aws_user_pools_id: 'ap-southeast-2_jtCfqBIS3',
+  aws_user_pools_web_client_id: '2mr9310lto338m4p5vv0jkasgs',
   oauth: {},
   aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
-  apiUrl: 'https://itly1o3ab7.execute-api.eu-central-1.amazonaws.com/latest'
+  apiUrl: 'https://ufvag71js5.execute-api.ap-southeast-2.amazonaws.com/latest'
 }
 
 export default config
